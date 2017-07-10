bac269737696859c1ca587e9b000dd0a64dcf651
Lee dong hyeun <jeddli@gmail.com>
2017-07-10 16:08:03 +0900

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTexbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTexbox.val()
     }, function() {
-
+        messageTexbox.val('');
     });
 });
 
@@ -47,12 +49,16 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported by your brower.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
