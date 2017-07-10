adb50fb748844f96dc95e611a477cdb028d93690
Lee dong hyeun <jeddli@gmail.com>
2017-07-10 14:11:47 +0900

    Add geolocation test

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,18 @@ socket.on('newMessage', function(message) {
     jQuery('#message').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {    
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+
+    li.append(a);
+    jQuery('#message').append(li);
+});
+
+
+
 // prevent refresh page..
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
@@ -26,5 +38,21 @@ jQuery('#message-form').on('submit', function (e) {
         text: jQuery('[name=message]').val()
     }, function() {
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported by your brower.');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location.');
     });
 });
